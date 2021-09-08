e2f3a0008a503782daa4886edcb0139570cdbfce
kim kiyong <kiyon.kimu@g.smartvalue.ad.jp>
2021-09-08 17:19:39 +0900

    add: data->reactive

---
+++ pages/data->reactive.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <ul>
+        <li v-for="u in data.users" :key="u.id">{{ u.id }} {{ u.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script lang="js">
+import { defineComponent, reactive, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+    setup() {
+    // object declare
+    const data = reactive({
+        users: [
+        { id: 1, name: '加藤かな' },
+        { id: 2, name: '田中紘一' },
+        { id: 3, name: '山田太郎' },
+        ],
+    })
+    // primitive declare
+    const title = ref("タイトル変更前")
+
+    setTimeout(() => {
+        data.users.push({ id: 4, name: '新藤誠' })
+        title.value = "変更後Title"
+    }, 3000);
+
+    return {
+        data,
+        title,
+    }
+  },
+})
+</script>
